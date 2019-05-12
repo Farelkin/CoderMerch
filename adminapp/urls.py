a44edf48173e1d7594a33f0bbe6fb65d702c5a4c
Ivan <Farelspam@yandex.ru>
2019-05-12 16:07:55 +0300

    some changes

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('', views.AdminView.as_view(), name='admin'),
-    path('admin_products/', views.products, name='admin_products'),
+    path('', views.AdminMainView.as_view(), name='admin'),
+    path('admin_products/', views.AdminProductsView.as_view(), name='admin_products'),
     path('admin_orders/', views.AdminOrdersView.as_view(), name='admin_orders'),
     path('admin_users/', views.AdminUsersView.as_view(), name='admin_users'),
 ]
+
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,31 +1,41 @@
-from products.models import ProductCategory, Product, ProductBySize, ProductImage
-from django.shortcuts import get_object_or_404, render
-from codermerch.settings import local_settings
-from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 
 
-def products(request):
-    title = 'CoderMerch | Заказы'
-
-    products_list = Product.objects.all()
-    print(products_list)
-    content = {
-        'title': title,
-        'products_list': products_list,
-
-    }
-
-    return render(request, 'adminapp/admin_products.html', content)
-
-
-class AdminView(TemplateView):
+class AdminMainView(TemplateView):
     template_name = 'adminapp/admin.html'
+
+    def get_context_data(self, **kwargs):
+        parent_context = super(AdminMainView, self).get_context_data(**kwargs)
+        parent_context['page_title'] = 'Админка | Главная'
+        parent_context['page_name'] = 'Админка сайта'
+        return parent_context
 
 
 class AdminOrdersView(TemplateView):
     template_name = 'adminapp/admin_orders.html'
 
+    def get_context_data(self, **kwargs):
+        parent_context = super(AdminOrdersView, self).get_context_data(**kwargs)
+        parent_context['page_title'] = 'Админка | Заказы'
+        parent_context['page_name'] = 'Заказы'
+        return parent_context
+
 
 class AdminUsersView(TemplateView):
     template_name = 'adminapp/admin_users.html'
+
+    def get_context_data(self, **kwargs):
+        parent_context = super(AdminUsersView, self).get_context_data(**kwargs)
+        parent_context['page_title'] = 'Админка | Юзеры'
+        parent_context['page_name'] = 'Юзеры'
+        return parent_context
+
+
+class AdminProductsView(TemplateView):
+    template_name = 'adminapp/admin_products.html'
+
+    def get_context_data(self, **kwargs):
+        parent_context = super(AdminProductsView, self).get_context_data(**kwargs)
+        parent_context['page_title'] = 'Админка | Товары'
+        parent_context['page_name'] = 'Товары'
+        return parent_context
