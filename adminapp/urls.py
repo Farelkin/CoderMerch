cd8a52d97979ffc919c4331f84858f859fee73e5
Ivan <Farelspam@yandex.ru>
2019-05-12 22:17:11 +0300

    работа с фильтрами в процессе

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -7,6 +7,8 @@ app_name = 'adminapp'
 urlpatterns = [
     path('', views.AdminMainView.as_view(), name='admin_main'),
     path('admin_products/', views.AdminProductsView.as_view(), name='admin_products'),
+    path('admin_products/category/<int:pk>/', views.AdminProductsView.as_view(), name='category'),
+    path('admin_products/gender/<int:pk>/', views.AdminProductsView.as_view(), name='gender'),
     path('admin_orders/', views.AdminOrdersView.as_view(), name='admin_orders'),
     path('admin_users/', views.AdminUsersView.as_view(), name='admin_users'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
-from products.models import Product, ProductImage
+from products.models import Product, ProductImage, ProductCategory
 
 class AdminMainView(TemplateView):
     template_name = 'adminapp/admin.html'
@@ -25,8 +26,19 @@ class AdminProductsView(TemplateView):
     template_name = 'adminapp/admin_products.html'
     model = Product
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self,  pk=None, **kwargs):
         parent_context = super(AdminProductsView, self).get_context_data(**kwargs)
+        links_menu = Product.objects.filter(gender=True)
+
+        if pk:
+            if pk == '0':
+                products = Product.objects.filter(category__is_active=True).order_by('price')
+                category = {'name': 'все'}
+            else:
+                category = get_object_or_404(ProductCategory, pk=pk)
+                products = \
+                    Product.objects.filter(category__pk=pk, category__is_active=True).order_by('price')
+
         parent_context = {
             'page_title': 'Админка | Товары',
             'page_name': 'Товары',
@@ -34,6 +46,8 @@ class AdminProductsView(TemplateView):
             'product_male_count': Product.objects.filter(gender='male').count(),
             'product_female_count': Product.objects.filter(gender='female').count(),
             'product_all_count': Product.objects.count(),
+            # 'category': category,
+            'links_menu': links_menu,
         }
         return parent_context
 
