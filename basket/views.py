2d059584e5981c3f7e7f337edb9bdf156587274e
Dmitry <41759353+dimchoo@users.noreply.github.com>
2019-05-19 04:31:51 +0300

    Basket, user order front
    * Добавил библиотеку для ввода телефонного номера
    
    * Добавил модалку оформления заказа пользователем
    
    * Добавид ссылку на открытие модалки
    
    * Добавил новые стили
    
    * Добавил скрипты маски ввода телефонного номера в шаблон
    
    * Добавил ссылку на страницу совершенного заказа
    
    * Написал view для шаблона оформленного заказа
    
    * Добавил новый шаблон в urls
    
    * Добавил новый шаблон (Информация после оформления заказа)
    
    * Добавил новый шаблон (Информация после оформления заказа)

--- basket/views.py
+++ basket/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class BasketView(TemplateView):
     template_name = 'basket/basket.html'
+
+
+class OrderDoneView(TemplateView):
+    template_name = 'basket/basket_user_order.html'
